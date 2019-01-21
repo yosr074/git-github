3b6b46db1b1b2a7e8ea35abfab8fdc31c19248e9
root <yosr.elyounsi@gmail.com>
2019-01-21 13:23:22 -0800

    java running

---
+++ test.java
@@ -0,0 +1,10 @@
+
+
+
+public class HelloWorld {
+  // Définition de la fonction main
+  public static void main(String[] arg){
+    // Affichage du commentaire Hello world dans la fenêtre de commande
+    System.out.println("Hello world");
+  }
+}
